4619b4772e1ce870ee88e828ff512d12920e1677
davesahaj <sahajdavework@pm.me>
2022-02-01 18:33:11 +0530

    redux w/o middleware

---
+++ src/common/apis/movieApiKey.js
@@ -0,0 +1,3 @@
+export const apiKey = "66a0418";
+
+// Here is your key: 66a0418 Please append it to all of your API requests, OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=66a0418 Click the following URL to activate your key: http://www.omdbapi.com/apikey.aspx?VERIFYKEY=b13eea27-9e30-4fe5-8bc6-c0f7e5b550f1 If you did not make this request, please disregard this email.
---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieDetails = () => {
+  return <div>Movie details</div>;
+};
+
+export default MovieDetails;
